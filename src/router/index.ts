573c930db8409d77b4a4d86c8a0d4ca19f883e56
Macbook M1 pro <macbookm1pro@canette.local>
2025-05-07 16:38:09 +0200

    Primitive style for login page

--- src/router/index.ts
+++ src/router/index.ts
@@ -2,11 +2,13 @@ import { createRouter, createWebHistory } from 'vue-router'
 import Home from '@/views/Home.vue'
 import Game from '@/views/Game.vue'
 import ChooseGameMode from '@/views/ChooseGameMode.vue'
+import Login from '@/views/Login.vue'
 
 const routes = [
 	{ path: '/', name: 'Home', component: Home },
 	{ path: '/gamemode', name: 'Gamemode', component: ChooseGameMode },
-	{ path: '/game', name: 'Game', component: Game }
+	{ path: '/game', name: 'Game', component: Game },
+	{ path: '/login', name: 'Login', component: Login}
 ]
 
 export default createRouter({
---
+++ src/views/Login.vue
@@ -0,0 +1,24 @@
+<script lang="ts" setup>
+// COURAGE
+</script>
+
+<template>
+	<div class="w-4/5 m-auto border p-4 flex flex-col mt-20 shadow-xl">
+		<h3 class="mb-3 text-center">Login</h3>
+		<div class="border flex flex-col p-4 space-y-3">
+			<form action="" class="flex flex-col justify-center space-y-3">
+				<label>
+					Username :
+					<input type="text" class="border-2 rounded h-8 w-full">
+				</label>
+				<label>
+					Password :
+					<input type="password" class="border-2 rounded h-8 w-full">
+				</label>
+				<input type="submit" class="py-2 px-5 border rounded text-blue-600 shadow-md cursor-pointer" value="Login">
+			</form>
+			<hr>
+			<button class="py-2 px-5 border rounded text-orange-400 shadow-md cursor-pointer">Google</button>
+		</div>
+	</div>
+</template>
