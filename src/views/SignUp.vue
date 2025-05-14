7a3972d31eb44d3ac125a1ebf2a162aed48fb6f5
Valentin Baud <vabaud@c1r4p10.42lehavre.fr>
2025-05-14 15:28:26 +0200

    add login and signup system

--- src/views/SignUp.vue
+++ src/views/SignUp.vue
@@ -5,33 +5,30 @@ import { onMounted, ref } from 'vue'
 
 const username = ref<string>('')
 const password = ref<string>('')
-const errors = ref<{ username?: string; password?: string}>({})
-const loginError = ref<string>('')
+const email = ref<string>('')
+const errors = ref<{username?: string; password?: string; email?: string}>({})
 
-const login = async () => {
+const signup = async () => {
     errors.value = {}
-
     if (!username.value){
         errors.value.username = "Le nom d'utilisateur est requis."
     }
     if (!password.value){
         errors.value.password = "Le mot de passe est requis."
     }
-    if (username.value && password.value)
-    {
-        const response = await axios.post('/api/auth/login', {
-            identifier: username.value,
+    if (!email.value){
+        errors.value.email = "L'email est requis."
+    }
+    if (email.value && password.value && username.value){
+        const response = await axios.post('/api/auth/signup', {
+            username: username.value,
             password: password.value,
+            email: email.value,
         })
-        console.log(response.data)
         localStorage.setItem('token', response.data.token)
+        console.log(response.data)
         router.push('/')
     }
-}
-
-const googleLogout = async() => {
-    const response = await axios.get('/api/auth/logout')
-    console.log(response.data)
 }
 
 const normalLogout = async () => {
@@ -48,27 +45,29 @@ const googleConnect = async() => {
 
 <template>
 	<div class="w-4/5 m-auto border p-4 flex flex-col mt-20 shadow-xl">
-		<h3 class="mb-3 text-center">Login</h3>
+		<h3 class="mb-3 text-center">Sign Up</h3>
 		<div class="border flex flex-col p-4 space-y-3">
-            <p v-if="loginError" class="text-red-600 text-sm mt-1">{{ loginError }}</p>
-			<form @submit.prevent="login" class="flex flex-col justify-center space-y-3">
+			<form @submit.prevent="signup" class="flex flex-col justify-center space-y-3">
 				<label>
 					Username :
 					<input v-model="username" type="text" class="border-2 rounded h-8 w-full">
                     <p v-if="errors.username" class="text-red-600 text-sm mt-1">{{ errors.username }}</p>
+				</label>
+                <label>
+					Email :
+					<input v-model="email" type="email" class="border-2 rounded h-8 w-full">
+                    <p v-if="errors.email" class="text-red-600 text-sm mt-1">{{ errors.email }}</p>
 				</label>
 				<label>
 					Password :
 					<input v-model="password" type="password" class="border-2 rounded h-8 w-full">
                     <p v-if="errors.password" class="text-red-600 text-sm mt-1">{{ errors.password }}</p>
 				</label>
-				<input type="submit" value="Login"
+				<input type="submit" value="Sign up"
 					   class="py-2 px-5 border rounded text-blue-600 shadow-md cursor-pointer">
 			</form>
 			<hr>
 			<button @click="googleConnect" class="py-2 px-5 border rounded text-orange-400 shadow-md cursor-pointer">Google</button>
-			<button @click="normalLogout" class="py-2 px-5 border rounded text-orange-400 shadow-md cursor-pointer">delog</button>
-			<button @click="googleLogout" class="py-2 px-5 border rounded text-orange-400 shadow-md cursor-pointer">delog google</button>
 		</div>
 	</div>
 </template>
